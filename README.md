80f711b5bfab8f16a960d3b5c23a43165c49485b
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-07-09 11:14:32 -0300

    Inicial commit.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # ciandt-components-breadcrumb
 Breadcrumb component written in angularjs.
+
+### Install
+
+* Install the dependency:
+
+   ```javascript
+   bower install ciandt-components-breadcrumb --save
+   ```
+* Add breadcrumb.js to your code:
+
+   ```html
+   <script src='assets/libs/ciandt-components-breadcrumb/breadcrumb.js'></script>
+   ```
+   - note that directory base used was assets/libs, change bower_components to assets/libs or move from bower_components to assets/libs with grunt.
+* Include module dependency:
+
+   ```javascript
+   angular.module('yourApp', ['ciandt.components.breadcrumb']);
+   ```
+======
+
+### How To Use
+
+1. **Add app-breadcrumb directive in your html**
+
+   ```html
+   <app-breadcrumb></app-breadcrumb>
+   ```
+2. **If necessary custom the default template, user templateUrl attribute to do this**
+
+   ```html
+   <app-breadcrumb templateUrl="app/common/components/breadcrumb/breadcrumb.html"></app-breadcrumb>
+   ```
+
+3. **Registry your rote navegation words**
+   Every route change success this directive update the variable $rootScope.appContext.breadcrumb, adding a array with all rote navegation words. Theses words should be setted on angular-route component.
+
+   ```html
+   $routeProviderReference
+      .when('/' + feature.urlPath, {
+         breadcrumb: ['System', 'Module', 'Feature'],
+         templateUrl: 'yourPage.html',
+         controller: 'yourController'
+      })));
+   ```
---
+++ breadcrumb.js
@@ -0,0 +1,46 @@
+﻿'use strict';
+
+define(['angular'], function () {
+
+    angular.module('ciandt.components.breadcrumb', []).constant('ciandt.components.breadcrumb.BreadcrumbConfig', {
+        homeTitle: 'Principal'
+    }).directive("appBreadcrumb", ['ciandt.components.breadcrumb.BreadcrumbConfig', function (BreadcrumbConfig) {
+        return {
+            restrict: 'E',
+            link: function (scope) {
+                // inicializa breadcrumb como página principal
+                if (!scope.$root.appContext) {
+                    scope.$root.appContext = {};
+                }
+                if (!scope.$root.appContext.breadcrumb) {
+                    scope.$root.appContext.breadcrumb = [BreadcrumbConfig.homeTitle];
+                }
+            },
+            templateUrl: function (elem, attrs) {
+                if (attrs.templateUrl) {
+                    return attrs.templateUrl;
+                } else {
+                    return "assets/libs/ciandt-components-breadcrumb/breadcrumb.html";
+                }
+            }
+        };
+    }]).run(['$rootScope', '$location', function ($rootScope, $location) {
+        // atualiza breadcrumb após evento de mudança de rota
+        $rootScope.$on('$routeChangeSuccess', function (ev, next, last) {
+            if (next && next.$$route) {
+                // atribui breadcrumb da página que a navegação está direcionando
+                if (next.$$route.breadcrumb) {
+                    if (!$rootScope.appContext) {
+                        $rootScope.appContext = {};
+                    }
+                    $rootScope.appContext.breadcrumb = next.$$route.breadcrumb;
+                }
+
+                // limpa querystring após mudança de rota, pra não ficar com lixo na url durante navegação
+                // utilizar injeção de $routeParams para obter parametros do querystring
+                $location.$$search = {};
+            }
+        });
+    }]);
+
+});
