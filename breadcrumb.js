d3e5ddefd42094641232e13bf177e51e64625c73
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-11-04 17:01:12 -0200

    internalize html and create package.json

--- breadcrumb.js
+++ breadcrumb.js
@@ -1,22 +1,28 @@
 ﻿'use strict';
 
-define(['angular'], function () {
+(function (factory) {
+    if (typeof define === 'function') {
+        define(["angular"], factory);
+    } else {
+        if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports){
+            module.exports = 'jedi.breadcrumb';
+        }
+        return factory();
+    }
+}(function() {
 
     var cancelListenerEvt;
 
     angular.module('jedi.breadcrumb', []).constant('jedi.breadcrumb.BreadcrumbConfig', {
-        homeTitle: 'Principal'
+        homeTitle: 'Home'
     }).directive("jdBreadcrumb", ['jedi.breadcrumb.BreadcrumbConfig', function (BreadcrumbConfig) {
         return {
             restrict: 'E',
             replace: true,
             link: function (scope, element) {
                 // inicializa breadcrumb como página principal
-                if (!scope.$root.appContext) {
-                    scope.$root.appContext = {};
-                }
-                if (!scope.$root.appContext.breadcrumb) {
-                    scope.$root.appContext.breadcrumb = [BreadcrumbConfig.homeTitle];
+                if (!scope.$root.jdBreadcrumb) {
+                    scope.$root.jdBreadcrumb = [BreadcrumbConfig.homeTitle];
                 }
 
                 scope.$on('$destroy', function () {
@@ -40,25 +46,27 @@ define(['angular'], function () {
                 }
             }
         };
-    }]).run(['$rootScope', '$location', 'jedi.breadcrumb.BreadcrumbConfig', function ($rootScope, $location, BreadcrumbConfig) {
+    }]).run(['$rootScope', '$location', 'jedi.breadcrumb.BreadcrumbConfig', '$templateCache', function ($rootScope, $location, BreadcrumbConfig, $templateCache) {
         // atualiza breadcrumb após evento de mudança de rota
         cancelListenerEvt = $rootScope.$on('$routeChangeSuccess', function (ev, next, last) {
             if (next && next.$$route) {
                 // atribui breadcrumb da página que a navegação está direcionando
                 if (next.$$route.breadcrumb) {
-                    if (!$rootScope.appContext) {
-                        $rootScope.appContext = {};
-                    }
-                    $rootScope.appContext.breadcrumb = next.$$route.breadcrumb;
+                    $rootScope.jdBreadcrumb = next.$$route.breadcrumb;
                 }
 
                 // limpa querystring após mudança de rota, pra não ficar com lixo na url durante navegação
                 // utilizar injeção de $routeParams para obter parametros do querystring
                 $location.$$search = {};
             } else {
-                $rootScope.appContext.breadcrumb = [BreadcrumbConfig.homeTitle];
+                $rootScope.jdBreadcrumb = [BreadcrumbConfig.homeTitle];
             }
         });
+
+        // template
+        $templateCache.put('assets/libs/ng-jedi-breadcrumb/breadcrumb.html', '<ol class="breadcrumb hidden-xs">'+
+                                                                             '    <li class="active" ng-repeat="item in jdBreadcrumb track by $index + item" jd-i18n>{{item}}</li>'+
+                                                                             '</ol>');
     }]);
 
-});
+}));
